9cb876490099a7cd9e7f6c30fc19a857e3ba991c
Karin Chung <KC@Macbooks-MacBook-Air.local>
2017-03-07 21:44:06 -0800

    Adding lesson 9 deliverable for match logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,15 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+
+/* if/else statement for when two cards are clicked on*/
+if (cardTwo === cardFour) {
+	alert("Sorry, try again.");
+}
+else if (cardOne === cardThree) {
+	alert("Sorry, try again.");
+}
+else {
+	alert("You found a match!");
+}
